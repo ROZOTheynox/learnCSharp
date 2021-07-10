9f01235b980b503cb65db3c9273170fa6c55444e
ROZO <asudraud.contact@gmail.com>
2021-07-10 17:50:01 +0200

    ROZO ep 1

---
+++ learnCSharp/learnCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnCSharp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string bite = "Ligne 1 \aLigne 2 \tLigne 3";
+            Console.WriteLine();
+        }
+    }
+}
