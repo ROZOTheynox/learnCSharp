93adf753b141533f5fadb05a48ca14a970b9248b
Theynox <theynxoff@gmail.com>
2021-07-11 18:55:00 +0200

    Csharp by theynox

---
+++ Premier test de Csharp/Personne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premier_test_de_Csharp
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+        public int age { get; set; }
+
+        public Personne(string Nom, string Prenom, int Age)
+        {
+            this.nom = Nom;
+            this.prenom = Prenom;
+            this.age = Age;
+        }
+
+
+        public string Info()
+        {
+            return nom + " " + prenom + " " + age;
+        }
+    }
+}
