689ccbd5661587b099c81e6a66434cc17377a751
ROZO <asudraud.contact@gmail.com>
2021-07-11 10:05:10 +0200

    ROZO ep 3

--- learnCSharp/learnCSharp/Program.cs
+++ learnCSharp/learnCSharp/Program.cs
@@ -11,18 +11,29 @@ namespace learnCSharp
        
         static void Main(string[] args)
         {
-            string username = "ROZO";
-            string password = "Jemangelecode1";
-
-            if(password == "Jemangelecode1" && username == "ROZO")
+            void direBonjour(string prenom)
             {
-                Console.WriteLine("Identifiants corrects");
-            }
-            else
-            {
-                Console.WriteLine("Identifiants non corrects");
+                Console.WriteLine("\t---------------");
+                Console.WriteLine("\tBonjour " + prenom);
+                Console.WriteLine("\t---------------");
             }
 
+            void direBonjourPlusieursFois(string prenom)
+            {
+                direBonjour(prenom);
+                direBonjour(prenom);
+                direBonjour(prenom);
+            }
+
+            direBonjourPlusieursFois("Alexis");
+
+            string aireRectangle(int longueur, int largeur)
+            {
+                int aire = longueur * largeur;
+                return "L'aire du rectangle est de " + aire;
+            }
+
+            Console.WriteLine(aireRectangle(5, 90));
         }
     }
 }
